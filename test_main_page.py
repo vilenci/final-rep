088cd610ad6ba84e33b9d66fb40258b112015b14
vilenci <45372550+vilenci@users.noreply.github.com>
2019-07-26 22:34:00 +0300

    Базовая страница для проекта: BasePage

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+   browser.get(link) 
+   go_to_login_page(browser)
+
