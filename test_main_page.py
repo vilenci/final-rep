7b3eca52c7744e6d187abd9f7ba1e0cb74307261
vilenci <45372550+vilenci@users.noreply.github.com>
2019-07-26 23:22:36 +0300

    тест на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,7 @@
+from pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
-
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser)
-
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
