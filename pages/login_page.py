36a1ea7085034a56452195630bb14c179cbb5677
vilenci <45372550+vilenci@users.noreply.github.com>
2019-07-27 02:22:08 +0300

    Реализация LoginPage и Переходы между страницами исправление ошибок

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+
 
 
 class LoginPage(BasePage):
@@ -9,7 +11,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "URL is not correct"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
